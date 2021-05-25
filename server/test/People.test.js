5e9235c0512f9c2c8da32262136a2813a480bfde
czarjulius <juliusczar.jc@gmail.com>
2021-05-25 10:31:13 +0100

    test endpoint for successful call

---
+++ server/test/People.test.js
@@ -0,0 +1,40 @@
+import supertest from 'supertest';
+import app from '../server';
+import { fetchPeopleQuery } from './query';
+import dummyPeople from './dummyPeople.json';
+import dummyHomeWorld from './dummyHomeWorld.json';
+import SwapiData from '../graphql/resources';
+
+
+const apolloClient = async ({ ...params } = {}) => await supertest(app).post('/graphql').set('Accept', 'application/json').send(params);
+
+const swapiApiMock = jest.fn(async () => {
+  return Promise.resolve(dummyPeople);
+});
+
+const getHomeWorldMock = jest.fn(async () => {
+  return Promise.resolve(dummyHomeWorld);
+});
+
+describe('Swapi Call', () => {
+    beforeAll(async () => {
+      jest.spyOn(SwapiData.prototype, 'request').mockImplementation(swapiApiMock);
+      jest.spyOn(SwapiData.prototype, 'getHomeWorld').mockImplementation(getHomeWorldMock);
+    });
+    afterAll(async () => {
+      jest.restoreAllMocks();
+    });
+    test('should fetch people successfully', async () => {
+      const { statusCode, body: { data: { fetchPeople } } } = await apolloClient({
+        query: fetchPeopleQuery,
+      });
+      expect(statusCode).toBe(200);
+      expect(fetchPeople.data).toHaveLength(2);
+      expect(fetchPeople).toHaveProperty('page');
+      expect(fetchPeople.data[0]).toHaveProperty('home_world');
+      expect(fetchPeople.data[0]).toHaveProperty('gender');
+      expect(fetchPeople.data[0]).toHaveProperty('height');
+      expect(fetchPeople.data[0]).toHaveProperty('mass');
+      expect(fetchPeople.data[0]).toHaveProperty('name');
+    });
+});
---
+++ server/test/query.js
@@ -0,0 +1,32 @@
+const fetchPeopleQuery = `
+query FetchPeople($filter: PageFilter) {
+  fetchPeople(filter: $filter) {
+    data {
+      name
+      height
+      mass
+      gender
+      home_world {
+        name
+        terrain
+        population
+        orbital_period
+        rotation_period
+        diameter
+        climate
+        gravity
+        surface_water
+      }
+    }
+    page{
+      total
+      next
+      previous
+    }
+  }
+}
+`
+
+module.exports = {
+  fetchPeopleQuery
+}
